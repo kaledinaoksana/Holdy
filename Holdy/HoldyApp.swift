cc8378ce7a4494cc86f2b03f4c69f7ddce1a42b1
Oksana <60602659+kaledinaoksana@users.noreply.github.com>
2022-07-12 15:19:16 +0400

    Initial Commit

---
+++ Holdy/HoldyApp.swift
@@ -0,0 +1,20 @@
+//
+//  HoldyApp.swift
+//  Holdy
+//
+//  Created by Oksana Kaledina on 12.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct HoldyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
